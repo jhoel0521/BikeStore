7381e93a058412cbaad1eafb08282c744f8591c3
Jhoel0521 <jhoel0521@gmail.com>
2024-07-12 10:42:23 -0400

    Separando la cadena de connection para que no este hard codeada

--- Models/BikeStoreContext.cs
+++ Models/BikeStoreContext.cs
@@ -6,10 +6,6 @@ namespace BikeStore.Models;
 
 public partial class BikeStoreContext : DbContext
 {
-    public BikeStoreContext()
-    {
-    }
-
     public BikeStoreContext(DbContextOptions<BikeStoreContext> options)
         : base(options)
     {
@@ -23,10 +19,7 @@ public partial class BikeStoreContext : DbContext
 
     public virtual DbSet<Product> Products { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-IH87ABI\\SQLEXPRESS;Initial Catalog=Bike_Store;Integrated Security=True;Trust Server Certificate=True");
-
+   
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Customer>(entity =>
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BikeStore.Data;
+using BikeStore.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,9 @@ namespace BikeStore
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+            // add context BikeStoreContext
+            builder.Services.AddDbContext<BikeStoreContext>(options =>
+                           options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
