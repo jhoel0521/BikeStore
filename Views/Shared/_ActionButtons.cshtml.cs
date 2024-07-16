652f2cd88136f67da0366b78befb8cad95d17362
Jhoel0521 <jhoel0521@gmail.com>
2024-07-16 10:18:37 -0400

    add _ActionButtons y estilos for Producto views

---
+++ Views/Shared/_ActionButtons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BikeStore.Views.Shared
+{
+    public class _ActionButtonsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
