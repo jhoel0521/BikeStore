3c6e1ee63eebf2997abec85da623f0c9a4c94910
jhoel0521 <SCPC501@labs.upds.bo>
2024-07-15 20:28:32 -0400

    Add all for prduct DataAnnotations

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BikeStore.Models;
 
 public partial class Product
 {
     public int ProductId { get; set; }
-
-    public string ProductName { get; set; } = null!;
-
+    [Display(Name = "Nombre del producto", Prompt = "Bicicleta turbo 3000")]
+    [Required(ErrorMessage = "El Nombre del producto es requedio")]
+    [StringLength(200, ErrorMessage = "El nombre del producto  no puede exeder los 50 caracteres")]
+    public string? ProductName { get; set; }
+    [Display(Name = "Año del producto", Prompt = "2024")]
+    [Required(ErrorMessage = "El año del modelo del producto es requedido")]
     public int ModelYear { get; set; }
-
+    [Display(Name ="Precio",Prompt ="99.99")]
+    [Column(TypeName = "decimal(10, 2)")]
+    [Required(ErrorMessage ="El Precio es requerido")]
     public decimal? Price { get; set; }
 
     public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
